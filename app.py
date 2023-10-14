144aa91e8d0539b38292ce57ce973de230f563ae
gustavopalaciosc <gpalaciosc@uc.cl>
2023-10-14 19:35:01 -0300

    Cambios busqueda backend/frontend

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
-from flask import Flask, render_template
-import subprocess
-from utils import scrape_antartica
+from flask import Flask, render_template, request
+from utils import scrape_general, scrape_antartica, scrape_buscalibre
 
 
 
@@ -10,11 +9,33 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route("/search",  methods=['GET', 'POST'])
+def search_book():
+    if request.method == 'POST':
+        # Procesa los datos del formulario aquí
+        titulo = request.form['title']
+        autor = request.form['author']
+        ans = [scrape_buscalibre(titulo, autor)]
+        # Realiza alguna acción con los datos, por ejemplo, mostrarlos en otra página
+        return render_template("price_comparison.html", books = ans)
+    return render_template('search.html')
+
+
+
+
+
+
+
+
+@app.route("/about")
+def about():
+    return render_template('about.html')
+
 @app.route('/scrape')
 def scrape():
     title = "guerra y paz"
     autor = "leon tolstoi"
-    ans = [scrape_antartica("guerra y paz", "leon tolstoi")]
+    ans = [scrape_buscalibre("guerra y paz", "leon tolstoi")]
     return render_template("price_comparison.html", books = ans)
 
     
